33ef62b3943b3704a421e838614969348976b41d
Lorena <lorena21395@gmail.com>
2020-06-04 10:35:40 -0400

    removed old versions of files

--- emcee_vel_fit.py
+++ emcee_vel_fit.py
@@ -8,33 +8,70 @@ from multiprocessing import Pool, cpu_count
 import emcee
 import corner
 from Corrfunc.theory.wp import wp
-import MCMC_data_file
+import zehavi_data_file_19,MCMC_data_file,guo_data_file
 from numpy.linalg import inv
 
 from warnings import simplefilter
-# ignore all future warnings
+#ignore all future warnings
 simplefilter(action='ignore', category=FutureWarning)
 
-wp_ng_vals = MCMC_data_file.get_wp()
-bin_edges = MCMC_data_file.get_bins()[0:28]
-cov_matrix = MCMC_data_file.get_cov()
-invcov = inv(cov_matrix[1:28,1:28])
-ng_cov = cov_matrix[0,0]
+import argparse
 
-wp_vals = wp_ng_vals[1:28]
-ng = wp_ng_vals[0]
+parser = argparse.ArgumentParser(description='data type')
+parser.add_argument('--file', type=str,
+                    help='Name of data file to fit')
+parser.add_argument('--catalog',type=str)
+parser.add_argument('--output',type=str)
+args = parser.parse_args()
 
-cens_occ_model = Zheng07Cens(prim_haloprop_key = 'halo_vmax')
+if args.file == "zehavi":
+    wp_ng_vals = zehavi_data_file_19.get_wp()
+    bin_edges = zehavi_data_file_19.get_bins()
+    cov_matrix = zehavi_data_file_19.get_cov()
+    invcov = inv(cov_matrix)
+    ng = wp_ng_vals[0]
+    ng_cov = 0.00007
+    wp_vals = wp_ng_vals[1:len(wp_ng_vals)]
+elif args.file == "fake":
+    wp_vals = MCMC_data_file.get_wp()
+    bin_edges = MCMC_data_file.get_bins()
+    cov_matrix = MCMC_data_file.get_cov()
+    invcov = inv(cov_matrix)
+    ng_cov = MCMC_data_file.get_ngcov()
+    ng = MCMC_data_file.get_ng()
+elif args.file == "guo":
+    wp_ng_vals = guo_data_file.get_wp()
+    bin_edges = guo_data_file.get_bins()
+    cov_matrix = guo_data_file.get_cov()
+    invcov = inv(cov_matrix)
+    ng_cov = 0.00003
+    wp_vals = wp_ng_vals[1:len(wp_ng_vals)]
+    ng = wp_ng_vals[0]
+
+#cens_occ_model = Zheng07Cens(prim_haloprop_key = 'halo_vmax')
+cens_occ_model = Zheng07Cens()
 cens_prof_model = TrivialPhaseSpace()
 
-sats_occ_model =  Zheng07Sats(prim_haloprop_key = 'halo_vmax')
+#sats_occ_model =  Zheng07Sats(prim_haloprop_key = 'halo_vmax',modulate_with_cenocc=True)
+sats_occ_model =  Zheng07Sats(modulate_with_cenocc=True)
 sats_prof_model = NFWPhaseSpace()
 
 model_instance = HodModelFactory(centrals_occupation = cens_occ_model,
                                  centrals_profile = cens_prof_model,
                                  satellites_occupation = sats_occ_model,
                                  satellites_profile = sats_prof_model)
-halocat = CachedHaloCatalog(simname='bolplanck',redshift = 0.0)
+
+if args.catalog == "bolplanck":
+    halocat = CachedHaloCatalog(fname = '/home/lom31/.astropy/cache/halotools/halo_catalogs/bolplanck/rockstar/hlist_1.00231.list.halotools_v0p4.hdf5',update_cached_fname = True)
+    halocat.redshift=0.
+elif args.catalog == "old":
+    halocat = CachedHaloCatalog(fname = '/home/lom31/Halo/hlist_1.00231.list.halotools_v0p1.hdf5',update_cached_fname = True)
+    halocat.redshift=0.
+elif args.catalog == "smdpl":
+    halocat = CachedHaloCatalog(fname = '/home/lom31/.astropy/cache/halotools/halo_catalogs/SMDPL/rockstar/2019-07-03-18-38-02-9731.dat.my_cosmosim_halos.hdf5',update_cached_fname = True)
+    halocat.redshift=0.
+elif args.catalog  == "mdr1":
+    halocat = CachedHaloCatalog(fname = '/home/lom31/.astropy/cache/halotools/halo_catalogs/multidark/rockstar/hlist_0.68215.list.halotools_v0p4.hdf5',update_cached_fname = True)
 model_instance.populate_mock(halocat)
 
 def lnlike(theta):
@@ -46,37 +83,44 @@ def lnlike(theta):
     model_instance.param_dict['logM0'] = logM0
     model_instance.param_dict['logM1'] = logM1
 
+    if args.catalog == 'smdpl':
+        Lbox = 400.
+    elif args.catalog == 'old':
+        Lbox = 250.
+    elif args.catalog == 'bolplanck':
+        Lbox = 250.
+    elif args.catalog == 'mdr1':
+        Lbox=1000.
     model_instance.mock.populate()
     table = model_instance.mock.halo_table
+
     pos = return_xyz_formatted_array(model_instance.mock.galaxy_table['x'],
                                      model_instance.mock.galaxy_table['y'],
-                                     model_instance.mock.galaxy_table['z'])
+                                     model_instance.mock.galaxy_table['z'], 
+                                     period = Lbox)
     x = pos[:,0]
     y = pos[:,1]
     z = pos[:,2]
     velz = model_instance.mock.galaxy_table['vz']
-    pos_zdist = return_xyz_formatted_array(x,y,z, velocity=velz,
+    pos_zdist = return_xyz_formatted_array(x,y,z,period=Lbox,velocity=velz,
                                 velocity_distortion_dimension='z')
     pi_max = 60.
-    Lbox = 250.
-    nthreads = 1
-    pos_zdist[:,2][np.where(pos_zdist[:,2]< 0.0)]= 0.0
-    pos_zdist[:,2][np.where(pos_zdist[:,2]> 250.0)]= 250.0
+    nthreads = 4
 
-    wp_calc = wp(Lbox,pi_max,nthreads,bin_edges,pos_zdist[:,0],pos_zdist[:,1],pos_zdist[:,2],verbose=False,xbin_refine_factor=3, ybin_refine_factor=3, zbin_refine_factor=2)
+    wp_calc = wp(Lbox,pi_max,nthreads,bin_edges,pos_zdist[:,0],pos_zdist[:,1],pos_zdist[:,2],verbose=False)#,xbin_refine_factor=3, ybin_refine_factor=3, zbin_refine_factor=2)
 
-    f2 = open('current_params.txt','w')
-    f2.write(str(theta))
-    f2.close()
+    #tot_gals = (np.sum(model_instance.mock.galaxy_table['halo_num_centrals'])+np.sum(model_instance.mock.galaxy_table['halo_num_satellites']))
+    #num_den = tot_gals/(Lbox**3)
 
     wp_diff = wp_vals-wp_calc['wp']
     ng_diff = ng-model_instance.mock.number_density
-    
-    return -0.5*np.dot(wp_diff, np.dot(invcov, wp_diff)) + -0.5*(ng_diff**2)*ng_cov
+
+    return -0.5*np.dot(wp_diff, np.dot(invcov, wp_diff)) + -0.5*(ng_diff**2)/(ng_cov**2)
 
 def lnprior(theta):
     logMmin, sigma_logM, alpha, logM0, logM1 = theta
-    if 1.<logMmin<3. and 0.2<sigma_logM<0.6 and 0.5<alpha<2.0 and 1.<logM0<3. and 1.0<logM1<4.:
+    #if 1.0<logMmin<4.0 and 0.01<sigma_logM<2.5 and 1.0<alpha<5.0 and 1.0<logM0<4.0 and 1.0<logM1<4.0:
+    if 10.0<logMmin<14.0 and 0.01<sigma_logM<2.5 and 0.85<alpha<5.0 and 10.0<logM0<14.0 and 10.0<logM1<14.0:     
         return 0.0
     return -np.inf
 
@@ -87,11 +131,12 @@ def lnprob(theta):
     return lp + lnlike(theta)
 
 ndim, nwalkers = 5, 100
-guess = [2.6, 0.4, 1.4, 2.3, 2.4]
+#guess = 2.46, 1.38, 2.73, 1.30, 2.34
+guess = 12.1, 1.0, 1.0, 10.96, 13.19
 pos = [guess + 1e-4*np.random.randn(ndim) for i in range(nwalkers)]
 # Set up the backend
 # Don't forget to clear it in case the file already exists
-filename = "samples_2.h5"
+filename = args.output
 backend = emcee.backends.HDFBackend(filename)
 backend.reset(nwalkers, ndim)
 
@@ -99,17 +144,12 @@ with Pool() as pool:
     sampler = emcee.EnsembleSampler(nwalkers, ndim, 
                                     lnprob, pool=pool,backend=backend)
     start = time.time()
-    sampler.run_mcmc(pos, 5000, progress=True, store=True)
+    sampler.run_mcmc(pos, 10000, progress=True, store=True)
     end = time.time()
     multi_time = end - start
     print("Multiprocessing took {0:.1f} seconds".format(multi_time))
-    f2 = open('multiprocessing_time.txt','w')
-    f2.write("Multiprocessing took {0:.1f} seconds".format(multi_time))
-    f2.close()
 
 samples = sampler.get_chain()
-f = open('param.txt','w')
-f.write(str(samples[np.where(sampler.lnprobability==sampler.lnprobability.max())]))
-f.close()
-
-print(samples[np.where(sampler.lnprobability==sampler.lnprobability.max())][0])
+#f = open('param.txt','w')
+#f.write(str(samples[np.where(sampler.lnprobability==sampler.lnprobability.max())]))
+#f.close()
