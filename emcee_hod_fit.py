7e6c45fa4a8094dc56594adb68a7362813917a05
Lorena <lorena21395@gmail.com>
2020-11-09 19:33:40 -0500

    just a regular update

--- emcee_hod_fit.py
+++ emcee_hod_fit.py
@@ -65,20 +65,21 @@ class fit_dict(dict):
                 wp_ng_vals = zehavi_data_file_19.get_wp()
                 bin_edges = zehavi_data_file_19.get_bins()
                 cov_matrix = zehavi_data_file_19.get_cov()
+                ng_err = 0.000068
             elif '20' in data_file:
                 import zehavi_data_file_20
                 wp_ng_vals = zehavi_data_file_20.get_wp()
                 bin_edges = zehavi_data_file_20.get_bins()
                 cov_matrix = zehavi_data_file_20.get_cov()
+                ng_err = 0.00007
             elif '21' in data_file:
                 import zehavi_data_file_21
-                wp_ng_vals = zehavi_data_file_21.get_wp()
-                bin_edges = zehavi_data_file_21.get_bins()
-                cov_matrix = zehavi_data_file_21.get_cov()
-
+                wp_ng_vals = zehavi_data_file_21.get_wp()[0:12]
+                bin_edges = zehavi_data_file_21.get_bins()[0:12]
+                cov_matrix = zehavi_data_file_21.get_cov()[0:11,0:11]
+                ng_err = 0.000005
             invcov = inv(cov_matrix)
             ng = wp_ng_vals[0]
-            ng_err = 0.00007
             wp_vals = wp_ng_vals[1:len(wp_ng_vals)]
 
             return wp_vals, ng, ng_err, bin_edges, invcov
@@ -117,21 +118,24 @@ class hod_fit(fit_dict):
             halocat = CachedHaloCatalog(fname = '/home/lom31/Halo/hlist_1.00231.list.halotools_v0p1.hdf5',update_cached_fname = True)
             halocat.redshift=0.
         elif self['sim']== "smdpl":
-            #halocat = CachedHaloCatalog(fname = '/home/lom31/.astropy/cache/halotools/halo_catalogs/SMDPL/rockstar/2019-07-03-18-38-02-9731.dat.my_cosmosim_halos.hdf5',update_cached_fname = True)
-            halocat = CachedHaloCatalog(fname='/home/lom31/.astropy/cache/halotools/halo_catalogs/smdpl/rockstar/2019-07-03-18-38-02-9731.dat.my_cosmosim_halos.hdf5',update_cached_fname = True)
+            halocat = CachedHaloCatalog(fname = '/home/lom31/.astropy/cache/halotools/halo_catalogs/SMDPL/rockstar/2019-07-03-18-38-02-9731.dat.my_cosmosim_halos.hdf5',update_cached_fname = True)
+            #halocat = CachedHaloCatalog(fname='/home/lom31/.astropy/cache/halotools/halo_catalogs/smdpl/rockstar/2019-07-03-18-38-02-9731.dat.my_cosmosim_halos.hdf5',update_cached_fname = True)
             halocat.redshift=0.
         elif self['sim'] == "mdr1":
             halocat = CachedHaloCatalog(fname = '/home/lom31/.astropy/cache/halotools/halo_catalogs/multidark/rockstar/hlist_0.68215.list.halotools_v0p4.hdf5',update_cached_fname = True)
         
         if self['param'] == 'mvir':
-            cens_occ_model = Zheng07Cens(threshold=-21)
+            cens_occ_model = Zheng07Cens(threshold=-19)
             cens_prof_model = TrivialPhaseSpace()
-            sats_occ_model =  Zheng07Sats(modulate_with_cenocc=True,threshold=-21)
+            sats_occ_model =  Zheng07Sats(modulate_with_cenocc=True,
+                                          threshold=-19)
             sats_prof_model = NFWPhaseSpace()
         elif self['param'] == 'vmax':
-            cens_occ_model = Zheng07Cens(prim_haloprop_key = 'halo_vmax')
+            cens_occ_model = Zheng07Cens(prim_haloprop_key = 'halo_vmax',
+                                         threshold=-19)
             cens_prof_model = TrivialPhaseSpace()
             sats_occ_model = Zheng07Sats(prim_haloprop_key = 'halo_vmax',
+                                         threshold=-19,
                                          modulate_with_cenocc=True)
             sats_prof_model = NFWPhaseSpace()
 
@@ -239,7 +243,7 @@ def main(args):
     logger = fd._get_log()
     backend = emcee.backends.HDFBackend(file_ext+filename)
     backend.reset(nwalkers, ndim)
-    with Pool(15) as pool:
+    with Pool(20) as pool:
             
         sampler = emcee.EnsembleSampler(nwalkers, ndim, hod._get_lnprob, 
                                         pool=pool,backend=backend)
